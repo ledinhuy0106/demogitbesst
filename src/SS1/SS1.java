0c18497ea7e8f0a758a4ae32bbadb1a4bcaff973
uyld@itsol.vn <uyld@itsol.vn>
2023-08-30 23:26:14 +0700

    123

--- src/SS1/SS1.java
+++ src/SS1/SS1.java
@@ -2,11 +2,9 @@ package SS1;
 
 public class SS1 {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("uys3");
-=======
         System.out.println("uy");
         System.out.println("uyss1");
->>>>>>> 94d5a1a (123)
     }
 }
